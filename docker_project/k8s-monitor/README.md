498765bb33394bf58c6b20d00cb21c0fea4ca13b
Everett <291394199@qq.com>
2018-09-21 18:27:26 +0800

    2018年9月21日18:27:10

--- docker_project/k8s-monitor/README.md
+++ docker_project/k8s-monitor/README.md
@@ -2,37 +2,42 @@
 
 本文章主要介绍如何全面监控k8s
 
-部署prometheus-operator
+使用metric-server收集数据给k8s集群内使用，如kubectl,hpa,scheduler等
 
-创建 namespace
+使用prometheus-operator部署prometheus，存储监控数据
+
+使用kube-state-metrics收集k8s集群内资源对象数据
+
+使用node_exporter收集集群中各节点的数据
+
+使用prometheus收集apiserver，scheduler，controller-manager，kubelet组件数据
+
+使用alertmanager实现监控报警
+
+使用grafana实现数据可视化
 
 kubectl apply -f monitoring-namespace.yaml
 
 kubectl apply -f prometheus-operator.yaml
 
-部署k8s组件服务
-
 kubectl apply -f node_exporter.yaml
-
-部署kube-state-metrics
 
 kubectl apply -f kube-state-metrics.yaml
 
-部署prometheus
-
 kubectl apply -f prometheus.yaml
-
-配置数据收集
 
 kubectl apply -f kube-servicemonitor.yaml
 
-部署grafana
+kubectl apply -f alertmanager.yaml
 
 kubectl apply -f grafana.yaml
 
-部署alertmanager
+注:alertmanager.yaml中的data是报警配置,需要重新创建secret
+创建保密字典
 
-kubectl apply -f alertmanager.yaml
+kubectl create  secret generic alertmanager-main --from-file=alerting_config.yaml
+
+替换alertmanager.yaml中的data,也就是保密字典中经过加密后的数据 
 
 参考文档:https://www.kubernetes.org.cn/4438.html
 
---
+++ docker_project/k8s-monitor/run.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#step 1 创建 namespace
+kubectl apply -f monitoring-namespace.yaml
+#step 2 部署 prometheus-operator
+kubectl apply -f prometheus-operator.yaml
+# 查看
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+kubectl get crd
+
+#step 3 部署 k8s组件服务
+kubectl apply -f node_exporter.yaml
+# 查看
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+
+#step 4
+kubectl apply -f kube-state-metrics.yaml
+# 查看
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+
+#step 5 部署prometheus
+kubectl apply -f prometheus.yaml
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+
+#step 6 配置数据收集
+kubectl apply -f kube-servicemonitor.yaml
+# 查看
+kubectl get servicemonitors -n monitoring
+
+# 查看 node port
+kubectl get svc -n monitoring | grep prometheus-k8s
+
+#step 7部署grafana
+kubectl apply -f grafana.yaml
+
+# 查看
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+
+# 查看 node port
+kubectl get svc -n monitoring | grep grafana
+
+#step 8 部署 alertmanager
+#创建保密字典
+cat > alerting_config.yaml << EOF
+global:
+  resolve_timeout: 5m
+route:
+  group_by: ['job']
+  group_wait: 30s
+  group_interval: 5m
+  repeat_interval: 5m
+  receiver: 'webhook'
+receivers:
+- name: 'webhook'
+  webhook_configs:
+  - url: 'http://192.168.145.1:8080/WebHookTest/'
+EOF
+
+kubectl create  secret generic alertmanager-main --from-file=alerting_config.yaml
+
+cat alertmanager.yaml | base64
+
+# 将加密后的数据添加到alertmanager.yaml
+
+kubectl apply -f alertmanager.yaml
+
+# 查看
+kubectl get pods -n monitoring
+kubectl get svc -n monitoring
+
+# 查看 node port
+
+kubectl get svc -n monitoring | grep alertmanager-main
