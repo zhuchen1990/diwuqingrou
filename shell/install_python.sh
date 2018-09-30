4ecb187a0dea4e7cb11f67c744d3c2cfca5dc375
Everett <291394199@qq.com>
2018-09-30 10:31:04 +0800

    CentOS7.5安装python3.6.6和python2.7.5并存

--- shell/install_python.sh
+++ shell/install_python.sh
@@ -5,11 +5,6 @@
 echo "下载python3.6.6"
 wget https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
 
-#备份python
-echo "备份python"
-cd /usr/bin/
-ll python*
-mv python python.bak
 
 #下载安装python需要的包
 echo "下载安装python需要的包"
@@ -18,23 +13,38 @@ yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel read
 #解压python
 echo "解压python"
 tar -zxvf Python-3.6.6.tgz
+
 #编译前的配置
 echo "编译前的配置"
 cd Python-3.6.6
 ./configure prefix=/usr/local/python3
+
 #编译
 echo "编译"
 make && make install
+
+#备份python
+echo "备份python"
+cd /usr/bin/
+mv python python.bak
+
 #生成python3的软连接
 echo "生成python3的软连接"
 ln -s /usr/local/python3/bin/python3 /usr/bin/python
+
 #查看python版本
 echo "查看python版本"
 python -V
+
 ##查看python2版本
-echo ""
+echo "查看python2版本"
 python2 -V
+
 #由于urlgrabber-ext-down和yum需要python2，所以将两个环境切换到python2
 echo "由于urlgrabber-ext-down和yum需要python2，所以将两个环境切换到python2"
-sed -i 's/\#\!\/usr\/bin\/python/\#\!\/usr\/bin\/python2/g'  /usr/libexec/urlgrabber-ext-down
-sed -i 's/\#\!\/usr\/bin\/python/\#\!\/usr\/bin\/python2/g'  /usr/bin/yum
+sed -i 's/\/usr\/bin\/python/\/usr\/bin\/python2/g'  /usr/libexec/urlgrabber-ext-down
+sed -i 's/\/usr\/bin\/python/\/usr\/bin\/python2/g'  /usr/bin/yum
+
+#查看是否将/usr/bin/python修改成了/usr/bin/python2
+cat /usr/libexec/urlgrabber-ext-down
+cat /usr/bin/yum
