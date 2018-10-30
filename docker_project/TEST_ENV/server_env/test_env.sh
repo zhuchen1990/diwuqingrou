1a177c3ac5a6a85d080a693299942ced9193bef2
第五轻柔 <291394199@qq.com>
2018-10-30 09:48:32 +0800

    test_env
    edit config

---
+++ docker_project/TEST_ENV/server_env/test_env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#实验环境7.0.0.13部署脚本
+#使用脚本前需将provider.zip,StarTrek.war,StarTrekEMR.war放到服务器
+#命令:sh -x test_env.sh
+#root_dir=/home/neo/project
+#使用前需要修改版本号
+#项目目录
+root_dir=/root/project
+version=4.0
+
+unzip provider.zip  && tar -zcvf provider.tar.gz provider/
+mv  provider.tar.gz $root_dir/pro/
+mv StarTrek.war $root_dir/his/
+mv StarTrekEMR.war $root_dir/emr/
+docker stop pro emr his
+docker rm  pro emr his
+#docker rmi chngenesis/pro:$version
+#docker rmi chngenesis/his:$version
+#docker rmi chngenesis/emr:$version
+docker build -t chngenesis/pro:$version $root_dir/pro/
+docker build -t chngenesis/his:$version $root_dir/his/
+docker build -t chngenesis/emr:$version $root_dir/emr/
+docker run -dit --name pro --restart=always --link=zookeeper  chngenesis/pro:$version
+sleep 120
+docker run -dit -p 8070:8080 --name his --restart=always --link=zookeeper chngenesis/his:$version
+docker run -dit -p 8072:8080 --name emr --restart=always --link=zookeeper chngenesis/emr:$version
+rm -rf provider.zip  && rm -rf provider/
--- docker_project/TEST_ENV/test_env.sh
+++ docker_project/TEST_ENV/test_env.sh
@@ -2,20 +2,24 @@
 #实验环境7.0.0.187部署脚本
 #使用脚本前需将provider.zip，StarTrek.war，StarTrekEMR.war放到服务器
 #命令:sh -x test_env.sh
-root_dir=/home/neo/project   
+#root_dir=/home/neo/project
+#使用前需要修改版本号
+#项目目录
+root_dir=/home/neo/project
+version=4.0
+
 unzip provider.zip  && tar -zcvf provider.tar.gz provider/
 mv  provider.tar.gz $root_dir/pro/
 mv StarTrek.war $root_dir/his/
 mv StarTrekEMR.war $root_dir/emr/
 docker stop pro emr his
 docker rm  pro emr his
-docker rmi chngenesis/pro:1.0
-docker rmi chngenesis/his:1.0
-docker rmi chngenesis/emr:1.0
-docker build -t chngenesis/pro:1.0 $root_dir/pro/
-docker build -t chngenesis/his:1.0 $root_dir/his/
-docker build -t chngenesis/emr:1.0 $root_dir/emr/
-docker run -dit --name pro --restart=always --link=zookeeper  --link=oracle --link=mongodb chngenesis/pro:1.0
-docker run -dit -p 8080:8080 --name his --restart=always --link=zookeeper chngenesis/his:1.0
-docker run -dit -p 8082:8080 --name emr --restart=always --link=zookeeper chngenesis/emr:1.0
+
+docker build -t chngenesis/pro:$version $root_dir/pro/
+docker build -t chngenesis/his:$version $root_dir/his/
+docker build -t chngenesis/emr:$version $root_dir/emr/
+docker run -dit --name pro --restart=always --link=zookeeper  --link=oracle --link=mongodb chngenesis/pro:$version
+sleep 120
+docker run -dit -p 8080:8080 --name his --restart=always --link=zookeeper chngenesis/his:$version
+docker run -dit -p 8082:8080 --name emr --restart=always --link=zookeeper chngenesis/emr:$version
 rm -rf provider.zip  && rm -rf provider/
